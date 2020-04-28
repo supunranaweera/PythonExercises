697b4c389c480abf6100b9ff850835b45b6ea1f8
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2020-04-27 21:11:45 -0600

    Created basics.py
    learned of variables and basic operators

---
+++ basics.py
@@ -0,0 +1,53 @@
+#  print a string
+print("hello world")
+
+# assigning values to variables
+
+x = 3
+
+y = 6
+
+z = 3.5
+
+message = "Hello there"
+
+# using advanced forms of the print funtions 
+
+print("value of x is", x)
+
+print("value of y is", y)
+
+print("value of z is", z,   "\nvalue of x is", x)
+
+
+
+#  all variabls have to start with an uppercase or lowercase letter, or an underscore, 
+#  followed by ny letters and/or an underscore
+#  a number can appear as anything other than the first letter
+
+
+
+# -----------BASIC OPERATORS-------------#
+
+sumValue = x + y
+
+
+print("sum of x and y is", x + y)
+
+print("subtraction of x and y is", x - y)
+
+
+
+
+k = 2
+
+print(x ** k)   # x to the power of k
+
+# * means multiply,   ** means exponent
+
+
+
+
+
+
+
