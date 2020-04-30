e490eb46807a44aa4712884a5b02981ddf0504f6
supunranaweera <57324451+supunranaweera@users.noreply.github.com>
2020-04-29 20:59:44 -0600

    logical and conditional statements

---
+++ conditional.py
@@ -0,0 +1,18 @@
+n = 0
+
+if n > 2:
+    print("n is bigger than 2")
+    print("n is big")
+else:
+    if n > 0:
+        print("n is small but positive")
+    else:
+        if n == 0:
+            print("n is 0")
+
+        print("n is negative")
+    print("n is small")
+
+print("n is a number")
+
+
---
+++ logical.py
@@ -0,0 +1,56 @@
+
+#  boolean values (logical values)
+
+b1 = True
+b2 = False
+
+
+
+print("b1 =", b1)
+
+print("b2 =", b2)
+
+
+
+x= b1 + b2
+
+n1 = 5
+n2 = 8
+n3 = n1 + n2
+
+
+result1 = n1 > 6
+result2 = n2 > 6
+
+print("result1 =", result1)
+print("result2=", result2)
+
+
+result3 = result1 or result2
+result4 = result1 and result2
+
+print(result3)
+print(result4)
+
+
+
+r1 = False and False
+r2 = False and True
+r3 = True and False
+r4 = True and True
+
+
+print("False or False =",r1)
+print("False or True =",r2)
+print("True  or False =",r3)
+print("True  or True =",r4)
+
+
+print("False and False =",r1)
+print("False and True =",r2)
+print("True  and False =",r3)
+print("True  and True =",r4)
+
+
+
+
